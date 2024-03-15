23646c86500f7d0ca237a7faad1eeb128683d730
mustafsahal24 <muabd4856@oslomet.no>
2024-03-15 15:35:39 +0100

    mjau

---
+++ src/main/java/com/example/oblig2webprog/Oblig2WebprogApplication.java
@@ -0,0 +1,13 @@
+package com.example.oblig2webprog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oblig2WebprogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oblig2WebprogApplication.class, args);
+    }
+
+}
